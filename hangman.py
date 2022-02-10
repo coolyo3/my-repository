ad50c5f97c0b2c953226070d5870550fcd1db770
coolyo3 <99376245+coolyo3@users.noreply.github.com>
2022-02-09 18:33:38 -0800

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,147 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import turtle
+import random
+
+#Drawing the head
+turtle.penup()
+turtle.goto(-70,100)
+turtle.pendown()
+turtle.circle(15)
+turtle.penup() 
+turtle.right(90)
+turtle.pendown()
+turtle.forward(50)
+turtle.penup
+turtle.right(45)
+turtle.pendown()
+turtle.forward(50)
+turtle.penup()  
+turtle.goto(-70,50)
+turtle.left(90)
+turtle.pendown()
+turtle.forward(50)
+turtle.penup()
+turtle.goto(-70,75)
+turtle.pendown()
+turtle.forward(50)
+turtle.penup()
+turtle.goto(-70,75)
+turtle.right(90)
+turtle.pendown()
+turtle.forward(50)
+turtle.penup()
+turtle.goto(-70,130)
+turtle.left(225)
+turtle.pendown()
+turtle.forward(25)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(175)
+turtle.right(90)
+turtle.forward(75)
+turtle.right(180)
+turtle.forward(150)
+#draw the body
+
+def drewperson(x):
+  guess = x
+  
+  if guess == 1:
+   #Drawing the head
+   turtle.penup()
+   turtle.goto(-70,100)
+   turtle.pendown()
+   turtle.circle(15)
+   turtle.penup()
+   turtle.right(90)
+  
+  #draw the body
+  elif guess == 2:
+    turtle.pendown()
+    turtle.forward(50)
+    turtle.penup
+    
+  elif guess ==3:
+    turtle.right(45)
+    turtle.pendown()
+    turtle.forward(50)
+    turtle.penup()  
+
+  elif guess == 4:
+    turtle.goto(-70,50)
+    turtle.left(45)
+    turtle.forward(50)
+    turtle.penup()
+
+  elif guess == 5:
+    turtle.goto(-70,75)
+    turtle.pendown()
+    turtle.forward(50)
+    turtle.penup()
+ 
+  elif guess == 6:
+    turtle.goto(-70,75)
+    turtle.right(90)
+    turtle.pendown()
+    turtle.forward(50)
+
+
+
+
+
+
+  turtle.hideturtle()
+  turtle.speed(0)
+  turtle.pensize(2)
+     
+
+
+
+
+
+  wordbank = ["dog","cat","bird","fish","bear","lizard","hippo","zebra","lion","tiger","eagle","wombat","beta","coral","manatee","dolphin","urchin","elephant",]
+
+
+
+
+
+while not False:
+ turtle.penup()
+ turtle.goto(-70,130)
+ turtle.left(225)
+ turtle.pendown()
+ turtle.forward(25)
+ turtle.right(90)
+ turtle.forward(50)
+ turtle.right(90)
+ turtle.forward(175)
+ turtle.right(90)
+ turtle.forward(75)
+ turtle.right(180)
+ turtle.forward(150)
+
+
+
+  
+
+    
+
+
+
+   
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
